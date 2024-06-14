23b615d05754ecb30b35f6d74e529cbb685d91db
iankimm <iankimmmmm@gmail.com>
2024-06-14 14:44:45 -0700

    restore, require auth

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -25,3 +25,41 @@ const setTokenCookie = (res, user) => {
 
   return token;
 };
+
+const restoreUser = (req, res, next) => {
+  // token parsed from cookies
+  const { token } = req.cookies;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
+      return next();
+    }
+
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope('currentUser').findByPk(id);
+    } catch (e) {
+      res.clearCookie('token');
+      return next();
+    }
+
+    if (!req.user) res.clearCookie('token');
+
+    return next();
+  });
+};
+
+const requireAuth = [
+  restoreUser,
+  function (req, _res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Unauthorized');
+    err.title = 'Unauthorized';
+    err.errors = ['Unauthorized'];
+    err.status = 401;
+    return next(err);
+  }
+];
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
