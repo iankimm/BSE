d95adc28441172b8b84622b60ab2311c9db04f9a
iankimm <iankimmmmm@gmail.com>
2024-06-28 16:59:39 -0700

    validation added to session.js

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,6 +4,9 @@ const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 router.post(
   '/',
   async (req, res, next) => {
@@ -35,5 +38,16 @@ router.delete(
     return res.json({ message: 'success' });
   }
 );
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
 const router = express.Router();
