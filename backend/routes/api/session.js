bc8241e50a2a48267e8b078a01b18f20cf8976e6
iankimm <iankimmmmm@gmail.com>
2024-07-12 17:52:46 -0700

    validate error happening

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,16 +39,16 @@ router.delete(
   }
 );
 
-const validateLogin = [
-  check('credential')
-    .exists({ checkFalsy: true })
-    .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
-  handleValidationErrors
-];
+// const validateLogin = [
+//   check('credential')
+//     .exists({ checkFalsy: true })
+//     .notEmpty()
+//     .withMessage('Please provide a valid email or username.'),
+//   check('password')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a password.'),
+//   handleValidationErrors
+// ];
 
 router.post(
   '/',
