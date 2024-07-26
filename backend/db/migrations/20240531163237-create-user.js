78074cf838cf4cf32549bff9daf9f86ca69bfa94
iankimm <iankimmmmm@gmail.com>
2024-07-26 13:14:19 -0700

    update

--- backend/db/migrations/20240531163237-create-user.js
+++ backend/db/migrations/20240531163237-create-user.js
@@ -21,7 +21,21 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
-
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      hashedPassword: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
     });
   },
